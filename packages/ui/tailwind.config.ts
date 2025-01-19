7833d522c4b87c03e8cd81d9812ad91d3ef91e89
Deepak Patankar <deepakpatankar7227@gmail.com>
2025-01-20 04:58:55 +0530

    Project Init with next.js & shadcn with tailwind

---
+++ packages/ui/tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss"
+import tailwindcssAnimate from "tailwindcss-animate";
+
+export default {
+  darkMode: ["class"],
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+     // Or if using `src` directory:
+     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
+  },
+  plugins: [tailwindcssAnimate],
+} satisfies Config
