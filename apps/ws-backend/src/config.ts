71861a898fefb8ec44d2b0202649d220f9c5025a
Deepak Patankar <deepakpatankar7227@gmail.com>
2025-04-07 21:36:58 +0530

    few more updates

--- apps/ws-backend/src/config.ts
+++ apps/ws-backend/src/config.ts
@@ -1,2 +1,3 @@
-
+import dotenv from "dotenv";
+dotenv.config();
 export const JWT_PASSWORD = process.env.JWT_PASSWORD
--- apps/ws-backend/src/middleware.ts
+++ apps/ws-backend/src/middleware.ts
@@ -2,11 +2,13 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_PASSWORD } from "./config";
 
+
 export function authMiddleware(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
+  
   const token = req.headers.authorization;
   if (!token) {
     res.status(401).json({
